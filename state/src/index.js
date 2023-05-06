b9939650b15a7f6923b8949d6ae81bf060a63724
RashmiMaxCoder <rashmibharti2021@gmail.com>
2023-05-06 19:28:44 +0530

    added state and lifecycle program

--- state/src/index.js
+++ state/src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+class State extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      date : new Date()
+    }
+  }
+  
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    ); 
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+
+  tick(){
+    this.setState({
+      date : new Date()
+    })
+  }
+  render(){
+    return (<h1>{this.state.date.toString()}</h1>);
+  };
+}
+
+ReactDOM.render(
+  <State name="Rashmi Bharti"/>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
